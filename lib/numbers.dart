ce256e396002859872da5ade2c1cdfa75ba91e83
joannapawlaczyk <joanna.pawlaczyk90@gmail.com>
2023-04-12 21:28:34 +0200

    Algorythm and first incomplete step

---
+++ lib/numbers.dart
@@ -0,0 +1,12 @@
+void main() {
+  /*
+  1. Pobrać liczbę od użytkownika
+  2. Porozdzielać na cyfry
+  3. Sprawdzic po kolei czy jest parzysta czy nie i je oznaczyć
+  4. wypisać oznaczone jako parzyste i nie
+  5. wypisać parzyste poprzedzielane + i na koncu = i potem suma
+  6. i to samo z nieparzystymi
+   */
+
+  print('Enter numbers');
+}
