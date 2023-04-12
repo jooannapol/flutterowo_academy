85f34edccd0970fcf9a1ec43674d26fd7f695452
joannapawlaczyk <joanna.pawlaczyk90@gmail.com>
2023-04-12 19:07:51 +0200

    Added entering numbers of rows

--- lib/project.dart
+++ lib/project.dart
@@ -1,11 +1,10 @@
 import 'dart:io';
 
 void main() {
-  int rows = 4;
-  for(int i = 0 ; i< rows; i++)
-  {
-    for(int j = 0; j<=i;j++)
-    {
+  stdout.write('Enter number of rows: ');
+  int rows = int.parse(stdin.readLineSync()!);
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j <= i; j++) {
       stdout.write('*');
     }
     stdout.writeln();
