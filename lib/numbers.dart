3edef82f7ddede4bc37ec6e28b5d43d72aea27c4
joannapawlaczyk <joanna.pawlaczyk90@gmail.com>
2023-04-13 14:54:11 +0200

    Added numbers to sum

--- lib/numbers.dart
+++ lib/numbers.dart
@@ -3,10 +3,9 @@ import 'dart:io';
 void main() {
   /*
   1. Pobrać liczbę od użytkownika DONE
-  2. Pobrac dlugosc ciagu przeslanego przez uzytkownika
-  3. Na podstawie dlugosci sprawdzanie w petli czy dana cyfra jest parzysta
-  3B. Sprawdzic po kolei czy jest parzysta czy nie i je oznaczyć
-  4. wypisać oznaczone jako parzyste i nie
+  2. Pobrac dlugosc ciagu przeslanego przez uzytkownika DONE
+  3. Na podstawie dlugosci sprawdzanie w petli czy dana cyfra jest parzysta i przypisać ją do zbioru liczb parzystych lub nie
+  4. Wydrukować liczby parzyste i niepatrzyste
   5. wypisać parzyste poprzedzielane + i na koncu = i potem suma
   6. i to samo z nieparzystymi
    */
@@ -44,4 +43,31 @@ void main() {
     }
   }
 
+  //printing sum of the even numbers:
+  stdout.write('Sum of even numbers: ');
+  int evenSum = 0;
+  for (int i = 0; i < evenNumbers.length; i++) {
+    if (i < evenNumbers.length - 1) {
+      stdout.write('${evenNumbers[i]}+');
+    } else {
+      stdout.write('${evenNumbers[i]}=');
+    }
+    int value = int.parse(evenNumbers[i]);
+    evenSum += value;
+  }
+  stdout.writeln(evenSum);
+
+  // printing sum of the odd numbers:
+  stdout.write('Sum of odd numbers: ');
+  int oddSum = 0;
+  for (int i = 0; i < oddNumbers.length; i++) {
+    if (i < oddNumbers.length - 1) {
+      stdout.write('${oddNumbers[i]}+');
+    } else {
+      stdout.write('${oddNumbers[i]}=');
+    }
+    int value = int.parse(oddNumbers[i]);
+    oddSum += value;
+  }
+  stdout.writeln(oddSum);
 }
