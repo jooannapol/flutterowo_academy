14c10f1a20469869b50ae7861046ea247004f532
joannapawlaczyk <joanna.pawlaczyk90@gmail.com>
2023-04-14 16:49:52 +0200

    Change function arguments into named

--- lib/numbers.dart
+++ lib/numbers.dart
@@ -10,8 +10,7 @@ void main() {
   6. i to samo z nieparzystymi
    */
 
-  print('Enter numbers');
-  String enteredNumbers = stdin.readLineSync() ?? '';
+  String enteredNumbers = collectNumber();
   String evenNumbers = '';
   String oddNumbers = '';
   for (int i = 0; i < enteredNumbers.length; i++) {
@@ -24,34 +23,42 @@ void main() {
     }
   }
 
-  void printListOfNumbers(String text, String numbers) {
-    stdout.write(text);
-    for (int i = 0; i < numbers.length; i++) {
-      if (i < numbers.length - 1) {
-        stdout.write('${numbers[i]},');
-      } else {
-        stdout.writeln(numbers[i]);
-      }
-    }
-  }
-
-  void printSumOfNumbers(String text, String numbers) {
-    stdout.write(text);
-    int sum = 0;
-    for (int i = 0; i < numbers.length; i++) {
-      if (i < numbers.length - 1) {
-        stdout.write('${numbers[i]}+');
-      } else {
-        stdout.write('${numbers[i]}=');
-      }
-      int value = int.parse(numbers[i]);
-      sum += value;
-    }
-    return stdout.writeln(sum);
-  }
-
-  printListOfNumbers('Even numbers: ', evenNumbers);
-  printListOfNumbers('Odd numbers: ', oddNumbers);
+  printListOfNumbers(text: 'Even numbers', numbers: evenNumbers);
+  printListOfNumbers(text: 'Odd numbers', numbers: oddNumbers);
   printSumOfNumbers('Sum of even numbers: ', evenNumbers);
   printSumOfNumbers('Sum of odd numbers: ', oddNumbers);
+}
+
+String collectNumber() {
+  stdout.writeln('Enter numbers');
+  String enteredNumbers = stdin.readLineSync() ?? '';
+  return enteredNumbers;
+}
+
+
+
+void printListOfNumbers({String text = ' ', required String numbers }) {
+  stdout.write(text);
+  for (int i = 0; i < numbers.length; i++) {
+    if (i < numbers.length - 1) {
+      stdout.write('${numbers[i]},');
+    } else {
+      stdout.writeln(numbers[i]);
+    }
+  }
+}
+
+void printSumOfNumbers(String text, String numbers) {
+  stdout.write(text);
+  int sum = 0;
+  for (int i = 0; i < numbers.length; i++) {
+    if (i < numbers.length - 1) {
+      stdout.write('${numbers[i]}+');
+    } else {
+      stdout.write('${numbers[i]}=');
+    }
+    int value = int.parse(numbers[i]);
+    sum += value;
+  }
+  return stdout.writeln(sum);
 }
