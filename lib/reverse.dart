4e58555c1f8bfe11ad48224d3d8a91a4e5f2e3ab
joannapawlaczyk <joanna.pawlaczyk90@gmail.com>
2023-04-14 11:53:15 +0200

    Proposition of algorythm, storing enetered text in a variable

---
+++ lib/reverse.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+/*
+- Wydrukowanie polecenia
+- Przechowanie wprowadzonego tekstu w zmiennej
+- zmiana pozycji pierwszego znaku na koniec w pętli
+- wydrukowanie rezultatu
+
+ */
+void main(){
+  stdout.write('Write a sentence to reverse ');
+  String enteredText = stdin.readLineSync()!;
+
+
+}
