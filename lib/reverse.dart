9231f8801d8d4c2dcb86a718a52cafced04d2a70
joannapawlaczyk <joanna.pawlaczyk90@gmail.com>
2023-04-15 18:42:17 +0200

    Row code

--- lib/reverse.dart
+++ lib/reverse.dart
@@ -3,13 +3,19 @@ import 'dart:io';
 /*
 - Wydrukowanie polecenia
 - Przechowanie wprowadzonego tekstu w zmiennej
-- zmiana pozycji pierwszego znaku na koniec w pętli
+- zmiana pozycji pierwszego znaku na koniec w pętli -> to nie ma sensu bo tekst będzie ten sam
+  abcd -> bcda -> cdab -> dabc -> abcd
 - wydrukowanie rezultatu
 
  */
 void main(){
   stdout.write('Write a sentence to reverse ');
   String enteredText = stdin.readLineSync()!;
+  String reversedText = '';
+  for(int i = enteredText.length - 1; i >= 0; i--){
+  reversedText += enteredText[i];
+  }
+  print(reversedText);
 
 
 }
