08b63157d9bebde5b027bac4a57dd053b4eb50e6
joannapawlaczyk <joanna.pawlaczyk90@gmail.com>
2023-04-16 15:25:32 +0200

    Stars pattern printing

--- lib/stars.dart
+++ lib/stars.dart
@@ -1,13 +1,20 @@
 import 'dart:io';
 
 void main() {
+  int rows = getNumberOfRows();
+  printStarsTreePattern(rows);
+}
+
+int getNumberOfRows(){
   stdout.write('Enter number of rows: ');
   int rows = int.parse(stdin.readLineSync()!);
-  for (int i = 0; i < rows; i++) {
-    for (int j = 0; j <= i; j++) {
-      stdout.write('*');
-    }
-    stdout.writeln();
+  return rows;
+}
+void printStarsTreePattern(int rows){
+  for (int i = 1; i <= rows; i++) {
+    String stars = "*" * (2*i - 1);
+    String spaces = " " * (rows - i);
+    stdout.writeln(spaces + stars);
   }
 }
 
