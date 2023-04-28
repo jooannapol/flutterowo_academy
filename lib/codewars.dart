a728a939f39dc981ee8543fe319d2707c975f1c1
joannapawlaczyk <joanna.pawlaczyk90@gmail.com>
2023-04-28 17:22:08 +0200

    Codewars Convert a String to a Number!

--- lib/codewars.dart
+++ lib/codewars.dart
@@ -1,10 +1,3 @@
 void main() {
-  num makeNegative(n) {
-    if (n>0){
-    return  -n;
-    }
-    else return n;
-  }
-
-  print(makeNegative(-9));
+  int stringToNumber(String str) => int.parse(str);
 }
