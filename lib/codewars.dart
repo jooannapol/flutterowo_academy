3569138d580503ec4d55576eb19a0cc0efa9925e
joannapawlaczyk <joanna.pawlaczyk90@gmail.com>
2023-04-29 17:36:38 +0200

    Codewars Reverse List Order

--- lib/codewars.dart
+++ lib/codewars.dart
@@ -1,9 +1,6 @@
 void main() {
-  String bool_to_word(bool boolean) {
-    if (boolean) {
-      return 'Yes';
-    } else {
-      return 'No';
-    }
+  List<int> reverseList(List<int> list) {
+    return list.reversed.toList();
   }
+  print(reverseList([1,2,3,4]));
 }
